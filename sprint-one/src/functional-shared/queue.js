9258c33ae289a4a71a6026868f48b42e509fdb71
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-27 15:48:34 -0700

    added count, size, and enqueue properties to the queue object

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = {count: 0};
+  jQuery.extend(obj, queueMethods);
+  return obj;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.count;
+  },
+  enqueue: function(value) {
+    this[this.size] = value;
+    this.count++;
+  },
+  dequeue: function() {}
+};
 
 
