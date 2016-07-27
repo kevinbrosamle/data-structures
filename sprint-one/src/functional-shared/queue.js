ddca6eda086b57e8028c1a9b4b8a884d596fbc62
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-27 15:58:11 -0700

    Added dequeue method.  Functional-shared implementation complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -13,10 +13,22 @@ var queueMethods = {
     return this.count;
   },
   enqueue: function(value) {
-    this[this.size] = value;
+    this[this.size()] = value;
     this.count++;
   },
-  dequeue: function() {}
+  dequeue: function() {
+    var temp = this[0];
+    delete this[0];
+    for (var i = 0; i < this.size(); i++) {
+      this[i] = this[i + 1];
+    }
+    delete this[this.size() - 1];
+    this.count--;
+    if (this.count < 0) {
+      this.count = 0;
+    }
+    return temp;
+  }
 };
 
 
