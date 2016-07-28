6ca09b156bf83c65eb751f6bdc1e02780fb3eeed
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-27 18:53:04 -0700

    completed pseudoclassical pattern for queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
+Queue.prototype.enqueue = function(value) {
+  this[this.size()] = value;
+  this.count++;
+};
+Queue.prototype.dequeue = function() {
+  var temp = this[0];
+  delete this[0];
+  for (var i = 0; i < this.size(); i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[this.size() - 1];
+  this.count--;
+  if (this.size() < 0) {
+    this.count = 0;
+  }
+  return temp;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
-
+Stack.prototype.size = function() {
+  return this.count;
+};
+Stack.prototype.push = function(value) {
+  this[this.size()] = value;
+  this.count++;
+};
+Stack.prototype.pop = function() {
+  var temp = this[this.size() - 1];
+  delete this[this.size() - 1];
+  this.count--;
+  if (this.size() < 0) {
+    this.count = 0;
+  }
+  return temp;
+};
