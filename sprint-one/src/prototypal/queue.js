02073924aac4082ed3da191389ab25513629d4aa
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-27 19:05:07 -0700

    notes to refactor

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,10 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //Note: in the future it will be better to store our queue in a seperate object.
   var obj = Object.create(queueMethods);
-  obj['count'] = 0;
+  obj.count = 0;
   return obj;
 };
 
