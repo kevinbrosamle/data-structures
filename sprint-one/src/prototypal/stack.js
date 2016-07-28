11f884bc51d09c3fcd2e4cfd55cca0ed64a82f5e
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-27 18:34:17 -0700

    Completed Prototypal patterns for Queue and Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj['count'] = 0;
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() { return this.count; },
+  push: function(value) {
+    this[this.size()] = value;
+    this.count++;
+  },
+  pop: function() {
+    var temp = this[this.size() - 1];
+    delete this[this.size() - 1];
+    this.count--;
+    if (this.size() < 0) {
+      this.count = 0;
+    }
+    return temp;
+  }
+};
 
 
