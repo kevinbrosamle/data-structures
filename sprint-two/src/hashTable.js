3bfce3dca3746564b807d5fe6b33edbcad0e2b8a
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-28 19:13:31 -0700

    Completed most of hashTable.  All that's left is to avoid conditions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,24 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
+  console.log(k);
+  console.log(index);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(val, i, collection) {
+    if (i === index) {
+      delete collection[i];
+    }
+  });
+  
 };
 
 
@@ -23,4 +33,8 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+// {John Smith: 532-2342, Lisa Smith: 234-2234 .... Sandra Dee: 743-3453}
 
+// {John Smith: 0, Lisa: 1 ... Sandra: 4}
+// [532-2342, 234-2234 .... 743-3453]
+//     0         1              4
