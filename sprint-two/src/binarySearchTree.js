3ee96fe4176bc421cdc3ea75080f4b1c8d87cdfe
kevinbrosamle <kevin.brosamle@gmail.com>
2016-07-29 11:42:53 -0700

    Completed insert and contains functions for binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,52 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(BinarySearchTree.prototype);
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
+  //Object.create(this.prototype);
 };
 
+BinarySearchTree.prototype.insert = function(val) {
+  var node = BinarySearchTree(val);
+  // this.left === null -> only if val is less than this.value
+  // this.right === null -> only if val is less than this.value
 
+  if (val > this.value) {
+    if (this.right === null) {
+      this.right = node;
+      return;
+    }
+    this.right.insert(val);
+  } else {
+    if (this.left === null) {
+      this.left = node;
+      return;
+    }
+    this.left.insert(val);
+  }
+};
+BinarySearchTree.prototype.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (target < this.value) {
+    //go left
+    if (this.left === null) {
+      return false;
+    }
+    return this.left.contains(target);
+  } else {
+    // go right
+    if (this.right === null) {
+      return false;
+    }
+    return this.right.contains(target);
+  }
+};
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
